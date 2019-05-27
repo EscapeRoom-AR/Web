8c6d06f1eb27066e8df334f7956a669ad5ac3da4
Alex <acuevasdev@gmail.com>
2019-05-27 20:10:13 +0200

    a

--- src/app/escape-room-module/carrousel/carrousel.component.ts
+++ src/app/escape-room-module/carrousel/carrousel.component.ts
@@ -11,17 +11,10 @@ export class CarrouselComponent implements OnInit {
 
   constructor() {
     this.images = [
-<<<<<<< HEAD
       "assets/img/about-us/IMG1.jpg",
       "assets/img/about-us/IMG2.jpg",
       "assets/img/about-us/IMG3.jpg",
-      "assets/img/about-us/IMG4.jpg",
-=======
-      "/assets/img/about-us/IMG1.jpg",
-      "/assets/img/about-us/IMG2.jpg",
-      "/assets/img/about-us/IMG3.jpg",
-      "/assets/img/about-us/IMG4.jpg",
->>>>>>> afd5e0538139af062ea46073dcac755f18ac9d34
+      "assets/img/about-us/IMG4.jpg"
     ]
   }
 
